7d587a299df195704bd4f25d883286761e44778c
Danil Gordienko <danilgordienko212@gmail.com>
2025-03-28 22:21:05 +0300

    initial commit

---
+++ src/main/java/ru/danilgordienko/film_storage/FilmStorageApplication.java
@@ -0,0 +1,13 @@
+package ru.danilgordienko.film_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/danilgordienko/film_storage/FilmStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.danilgordienko.film_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
