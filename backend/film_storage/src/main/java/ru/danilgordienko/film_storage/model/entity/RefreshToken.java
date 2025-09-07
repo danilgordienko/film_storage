571901312af1486357769b836d710f686089f94a
danilgordienko <danilgordienko212@gmail.com>
2025-09-07 15:34:03 +0300

    добавил миграции на добавление админа и длины токенов

--- backend/film_storage/src/main/java/ru/danilgordienko/film_storage/model/entity/RefreshToken.java
+++ backend/film_storage/src/main/java/ru/danilgordienko/film_storage/model/entity/RefreshToken.java
@@ -18,7 +18,7 @@ public class RefreshToken {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, columnDefinition = "TEXT")
     private String token;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
